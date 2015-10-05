206452a940394c64d7aea8ea04fd8af97eadcf3a
Mikael Johansson <c10mjn@cs.umu.se>
2015-10-05 10:52:02 +0200

    Communication, GroupManagement packages added.

---
+++ src/se/umu/cs/_5dv147_proj/communication/module/CommunicationModule.java
@@ -0,0 +1,9 @@
+package se.umu.cs._5dv147_proj.communication.module;
+
+/**
+ * Created by c10mjn on 2015-10-05.
+ */
+public class CommunicationModule {
+    public CommunicationModule() {
+    }
+}
---
+++ src/se/umu/cs/_5dv147_proj/groupmanagement/module/GroupModule.java
@@ -0,0 +1,9 @@
+package se.umu.cs._5dv147_proj.groupmanagement.module;
+
+/**
+ * Created by c10mjn on 2015-10-05.
+ */
+public class GroupModule {
+    public GroupModule() {
+    }
+}
